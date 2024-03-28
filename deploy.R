04a15b3cc32b7f7f671de938eb2c01f061028ced
Harmeet <hs978698@gmail.com>
2024-03-28 12:28:26 +0530

    new

---
+++ deploy.R
@@ -0,0 +1,20 @@
+deploy in read.table('filename', header = TRUE,  sep = '	',  stringsAsFactors = FALSE)
+library(dplyr)
+?dplyr
+
+
+iris
+View(iris)
+str(cars)
+View(cars)
+str(airquality)
+View(airquality)
+str(freeny)
+View(freeny)
+
+str(LifeCycleSavings)
+str(mpg)
+str(mtcars)
+str(ToothGrowth)
+str(USArrests)
+str(ChickWeight)
