dde817ae5564950a772e571c0215484e26794d91
Harmeet <hs978698@gmail.com>
2024-08-22 15:30:18 +0530

    overview of lists

--- listmatrixvector.R
+++ listmatrixvector.R
@@ -22,8 +22,23 @@ vector4 <- c(1+2i, 2+3i, 3+4i)
 list1 <- list(vector1, vector2, vector3, vector4)
 names(list1) <- c("numeric", "char", "logical", "complex")
 list1
-list1$vector1
+#2nd value of char
+list1$vec1[2]
+#add new index
+list1$Gender <- c("M", "F", "M")
+list1
+#remove index
+list1$Gender <- NULL
+list1
 
 
 list2 <- list(numeric=vector1, char=vector2, logical=vector3, complex=vector4, list_name=list1)
 list2
+
+#user defined list
+a <- as.numeric(readline(prompt = "Enter an integer value = "))
+b <- as.logical(readline(prompt = "Enter a logical value = "))
+d <- as.complex(readline(prompt = "Enter a complex value = "))
+c <- as.character(readline(prompt = "Enter a character value = "))
+list <- list("Integer"=a, "Logical"=b, "Char"=c, "Complex"=d)
+list
