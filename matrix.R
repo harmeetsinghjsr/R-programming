9b27855ce4721768394813251d0fd7612e604c85
Harmeet <hs978698@gmail.com>
2024-08-22 23:24:26 +0530

    Matrix name dim etc

--- matrix.R
+++ matrix.R
@@ -1,4 +1,23 @@
-mm <- matrix(c('a','b','c','b','a','c','b','c','a'),3, 3, FALSE)
+mm <- matrix(1:9,3, 3, FALSE)
+colnames(mm) <- c("row1", "row2", "row3")
+rownames(mm) <- c("col1", "col2", "col3")
 print(mm)
 
-add alternate matrix elemen
+row_names <- c("row1", "row2", "row3")
+col_names <- c("col1", "col2", "col3")
+rn <- matrix(1:9, 3, 3, TRUE, dimnames = list(row_names, col_names))
+print(rn)
+
+
+rn[1:2,1:2] <- 10
+print(rn)
+
+rn[,c(1,3)] <- 100
+print(rn)
+
+rn[1[2],3[1]] <- 2
+print(rn)
+
+#3d arrayarray1
+array1 <- array(1:24, dim = c(3, 4, 2))
+print(array1)
