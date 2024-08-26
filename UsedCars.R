f85eaa2d027934a49def11bd0b34691eef4fbf80
Harmeet <hs978698@gmail.com>
2024-08-26 15:45:46 +0530

    used cars dataset and summarising

---
+++ UsedCars.R
@@ -0,0 +1,22 @@
+usedcars<-read.csv("C:/Users/hs978/CodeSpace/R-programming/usedcars.csv")
+View(usedcars)
+str(usedcars)
+summary(usedcars$color,usedcars$price)
+
+summary(usedcars[c("color","price")])
+#five parts
+quantile(usedcars$price)
+#six parts
+quantile(usedcars$price,probs = c(0,0.2,0.4,0.6,0.8,1))
+quantile(usedcars$price,seq(0,1,0.2))
+quantile(usedcars$price,seq(0,1,length.out=6))
+#table
+table(usedcars$color)
+#table with percentage
+prop <- prop.table(table(usedcars$model))#these values are determined by the number of rows in the dataset
+round(prop,digits=2)
+#round in percentage
+library(scales)
+# table with percentage
+prop_percent <- percent(prop, accuracy = 0.01) # round to 2 decimal places and display as percentages
+prop_percent
