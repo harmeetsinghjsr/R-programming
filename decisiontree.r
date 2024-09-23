428aca0fb1dfb9def0ce63103eebffe8fe314668
Harmeet <hs978698@gmail.com>
2024-09-23 15:18:28 +0530

    entropy

---
+++ decisiontree.r
@@ -0,0 +1,16 @@
+df<- data.frame(
+  Day = c('D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14'),
+  Outlook = c('Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny', 'Sunny', 'Rain', 'Sunny', 'Overcast', 'Overcast', 'Rain'),
+  Temp = c('Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild'),
+  Humidity = c('High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'High'),
+  Wind = c('Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Strong'),
+  Play = c('No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No')
+)
+View(df)
+
+entropy<-function(x){
+  p<-table(x)/length(x)
+  -sum(p * log2(p))
+}
+
+sapply(df[,2:5], entropy)
